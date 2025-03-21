e70e872d36702bf6c8f9a5a302aacc0977280e49
DaVinci34162 <d.gisanura@alustudent.com>
2025-03-21 13:34:32 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """API"""
    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "MyRedditBot/1.0"}
-    response = requests.get(reddit_url, headers=headers)
+   response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()['data']
