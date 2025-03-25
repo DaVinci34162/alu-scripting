3cec3324833b4d78fe9b0bc2b05015714649709e
DaVinci34162 <d.gisanura@alustudent.com>
2025-03-25 13:13:57 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""API"""
+"""Fetches the top 10 hot posts of a given subreddit using Reddit API"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """API"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """Prints the titles of the top 10 hot posts of a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        data = response.json().get("data", {}).get("children", [])
+        if data:
+            for post in data:
+                print(post["data"]["title"])
+        else:
+            print("None") 
     else:
-        print(None)
+        print("None")  
